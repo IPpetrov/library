0cd2f82afadf2f61bb7c7ee529ab361fcfdc4e6a
IPpetrov <i.petrow@live.com>
2023-07-20 16:14:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ const myLibrary = [];
 
 // Book constructor
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 // Adding books to array
